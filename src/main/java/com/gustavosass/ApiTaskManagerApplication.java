1fc5469d9d9643065ca99c9d412fef484a412118
Gustavo Sass <gustavosass@gmail.com>
2024-10-04 14:28:51 -0300

    First commit.
    Signed-off-by: Gustavo Sass <gustavosass@gmail.com>

---
+++ src/main/java/com/gustavosass/ApiTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gustavosass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTaskManagerApplication.class, args);
+	}
+
+}
