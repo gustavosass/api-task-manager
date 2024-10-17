637eedf17fc2a391967de09b1347dbdd4849aa49
Gustavo Sass <gustavosass@gmail.com>
2024-10-17 16:49:52 -0300

    Fix: CORS
    Signed-off-by: Gustavo Sass <gustavosass@gmail.com>

--- src/main/java/com/gustavosass/ApiTaskManagerApplication.java
+++ src/main/java/com/gustavosass/ApiTaskManagerApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication()
 public class ApiTaskManagerApplication {
 
